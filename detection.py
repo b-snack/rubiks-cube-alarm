167ae0f8a4a0a5751835acaa4df7a39d45a08fc0
bsnack <swuott2009@gmail.com>
2026-01-07 19:15:50 -0500

    works but there are a lot of logic errors in is_solved(frame)

--- detection.py
+++ detection.py
@@ -2,6 +2,16 @@ import cv2 as cv
 import numpy as np
 
 frame = cv.imread('Photos/cube2.png') #using placehodler for now.
+
+  #mask (no white)
+COLOR_INFO = {
+  'blue': (np.array([100,150,0]), np.array([140,255,255])),
+  'green': (np.array([100,25,25]), np.array([80,255,255])), # or 40,50,50,80,255,255
+  'orange': (np.array([12, 150, 100]), np.array([18, 255, 255])),
+  'yellow': (np.array([22,100,100]), np.array([38,255,255])),
+  'red': (None, None)
+}
+
 # actual thing will need to take a photo first.
 
 def is_red(color_name, image):
@@ -20,22 +30,13 @@ def is_red(color_name, image):
 def is_there(frame):
   img = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-  #mask (no white)
-  color_info = {
-    'blue': (np.array([100,150,0]), np.array([140,255,255])),
-    'green': (np.array([100,25,25]), np.array([80,255,255])), # or 40,50,50,80,255,255
-    'orange': (np.array([12, 150, 100]), np.array([18, 255, 255])),
-    'yellow': (np.array([22,100,100]), np.array([38,255,255])),
-    'red': (None, None)
-  }
-
   #singificant pixels:
   colors_found = 0
   threshold = 500
   kernel = np.ones((5,5), np.uint8)
 
   # count num of colors present
-  for color_name, (lower, upper) in color_info.items():
+  for color_name, (lower, upper) in COLOR_INFO.items():
 
     if color_name == "red":
       mask = is_red(color_name, img)
@@ -59,8 +60,56 @@ def is_there(frame):
 
   return return_value
 
+def is_solved(frame):
+  gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+  blurred = cv.GaussianBlur(gray, (5,5), 0)
+  edges = cv.Canny(gray, 50, 150)
+
+  cv.imshow("Canny edges", edges)
+  
+  contours, _ = cv.findContours(edges, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+  min_area = 2000
+  threshold = 500
+  kernel = np.ones((5,5), np.uint8)
+  solved_sides = 0
+
+  for contour in contours:
+    perimeter = cv.arcLength(contour, 1)
+    approx = cv.approxPolyDP(contour, 0.02 * perimeter, True)
+
+    if len(approx) == 4 and cv.contourArea(contour) > min_area:
+      blank = np.zeros(frame.shape[:2], dtype='uint8')
+      cv.drawContours(blank, [approx], 0,255, -1)
+
+      side_area = cv.bitwise_and(frame, frame, mask=blank)
+
+      for color_name, (lower, upper) in COLOR_INFO.items():
+
+        if color_name == "red":
+          mask = is_red(color_name, frame)
+        else:
+          mask = cv.inRange(frame, lower, upper)
+
+        masked = cv.bitwise_and(frame, frame, mask=mask)
+
+        opening = cv.morphologyEx(mask, cv.MORPH_OPEN, kernel)
+        final_mask = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+
+        if cv.countNonZero(final_mask) >= 0.85*cv.countNonZero(side_area):
+          solved_sides += 1
+
+    return_value = False
+    if solved_sides == 3:
+      return_value = True
+    
+    return return_value
+
 result = is_there(frame)
 print(result)
+
+result1 = is_solved(frame)
+print(is_solved(frame))
 
 cv.waitKey(0)
 cv.destroyAllWindows()
