e7dca203d3154d3e1bdeceb88a7d8b568507a698
bsnack <swuott2009@gmail.com>
2026-01-09 10:15:59 -0500

    changes to make compatible with raspberry pi

--- alarm.py
+++ alarm.py
@@ -1,11 +1,17 @@
-import pygame
+import subprocess
 
 def play_alarm():
-  pygame.mixer.init()
-  pygame.mixer.music.load("media/alarm.mp3")
-  pygame.mixer.music.play(loops=-1)
+  subprocess.Popen("cvlc", "--play-and-exit", "--loop", "media/alarm.mp3")
 
 def stop_alarm():
-  pygame.mixer.music.stop()
+  subprocess.call("pkill", "vlc")
+
+# def play_alarm():
+#   pygame.mixer.init()
+#   pygame.mixer.music.load("media/alarm.mp3")
+#   pygame.mixer.music.play(loops=-1)
+
+# def stop_alarm():
+#   pygame.mixer.music.stop()
 
 # os.system("cvlc --play-and-exit alarm.mp3 &") for pi
--- capture.py
+++ capture.py
@@ -1,17 +1,20 @@
 import cv2 as cv
-import random
 
 def photo():
-  cap = cv.VideoCapture(0)
-  ret,frame = cap.read() 
-  return_value = None
+  cap = cv.VideoCapture(0, cv.CAP_V4L2)
 
+  for i in range(50):
+    cap.read()
+  
+  ret,frame = cap.read() 
   cap.release()
+
+  return_value = None
   
   if ret:
     return_value = frame
   
-  return return_value
+  return return_value 
     
 
 """
